45da12885332f4857ae6c657e3eebad0ea49c8f5
arvndblocker"nn <aravindvasang@gmail.com>
2023-10-13 20:14:36 +0530

    dapp_prototype

--- Main.py
+++ Main.py
@@ -1,6 +1,7 @@
 from Transaction import Transaction
 from Wallet import Wallet
 from TrasactionPool import TransitionPool
+from Block import Block
 
 if __name__ == "__main__":
     sender = "sender"
@@ -8,29 +9,14 @@ if __name__ == "__main__":
     amount = 1
     type = "TRANSFER"
 
-    # transaction = Transaction(sender, receiver, amount, type)
-    # wallet = Wallet()
-    # signature = wallet.sign(transaction.toJson())
-    # transaction.sign(signature)
-    # signatureValid = Wallet.signatureValid(
-    #     transaction.payload(), signature, wallet.publicKeyString()
-    # )
-    # print(signatureValid)
-
     wallet = Wallet()
     fradulantWallet = Wallet()
     pool = TransitionPool()
 
     transaction = wallet.createTransaction(receiver, amount, type)
 
-    # signatureValid = Wallet.signatureValid(
-    #     transaction.payload(), transaction.signature, fradulantWallet.publicKeyString()
-    # )
-
     if pool.transactionExists(transaction) == False:
         pool.addTransaction(transaction)
 
-    if pool.transactionExists(transaction) == False:
-        pool.addTransaction(transaction)
-
-    print(pool.transactions)
+    block = wallet.createBlock(pool.transactions, "lastHash", 1)
+    print(block.toJson())
