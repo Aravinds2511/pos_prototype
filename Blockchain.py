4c5e6037ee8d67602f0dc330162ea8c5cd5b6ea7
arvndblocker"nn <aravindvasang@gmail.com>
2023-10-14 15:55:35 +0530

    pos_prototype

--- Blockchain.py
+++ Blockchain.py
@@ -1,6 +1,10 @@
+from Block import Block
+from BlockchainUtils import BlockchainUtils
+
+
 class Blockchain:
     def __init__(self):
-        self.blocks = []
+        self.blocks = [Block.genesis()]
 
     def addBlock(self, block):
         self.blocks.append(block)
@@ -12,3 +16,18 @@ class Blockchain:
             jsonBlocks.append(block.toJson())
         data["blocks"] = jsonBlocks
         return data
+
+    def blockCountValid(self, block):
+        if self.blocks[-1].blockCount == block.blockCount - 1:
+            return True
+        else:
+            return False
+
+    def lastBlockHashValid(self, block):
+        latestBlockchainBlockHash = BlockchainUtils.hash(
+            self.blocks[-1].payload()
+        ).hexdigest()
+        if latestBlockchainBlockHash == block.lastHash:
+            return True
+        else:
+            return False
