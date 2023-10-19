ee10b7e2c385fbad40cac9f99fd0bf30f2a12793
arvndblocker"nn <aravindvasang@gmail.com>
2023-10-19 15:42:24 +0530

    selecting_forger

--- Test.py
+++ Test.py
@@ -1,12 +1,29 @@
 from ProofOfStake import ProofOfStake
 from Lot import Lot
+import string
+import random
+
+
+def getRandomString(length):
+    letters = string.ascii_lowercase
+    resultString = "".join(random.choice(letters) for i in range(length))
+    return resultString
+
 
 if __name__ == "__main__":
-    # pos = ProofOfStake()
-    # pos.update("bob", 10)
-    # pos.update("alice", 100)
-    # print(pos.get("bob"))
-    # print(pos.get("alice"))
-    # print(pos.get("john"))
-    lot = Lot("bob", 5, "lastHash")
-    print(lot.lotHash())
+    pos = ProofOfStake()
+    pos.update("alice", 10)
+    pos.update("bob", 100)
+
+    bobWins = 0
+    aliceWins = 0
+
+    for i in range(100):
+        forger = pos.forger(getRandomString(i))
+        if forger == "alice":
+            aliceWins += 1
+        elif forger == "bob":
+            bobWins += 1
+
+    print("bob won: " + str(bobWins) + " times")
+    print("alice won: " + str(aliceWins) + " times")
