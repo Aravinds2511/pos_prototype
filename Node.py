f1b256ce7d01a8d4e04c292fc268c4f0b11dfaf2
arvndblocker"nn <aravindvasang@gmail.com>
2023-10-20 12:34:55 +0530

    forging_blocks

--- Node.py
+++ Node.py
@@ -33,7 +33,8 @@ class Node:
         signerPublicKey = transaction.senderPublicKey
         signatureValid = Wallet.signatureValid(data, signature, signerPublicKey)
         transactionExists = self.transactionPool.transactionExists(transaction)
-        if not transactionExists and signatureValid:
+        transactionInBlock = self.blockchain.transactionExists(transaction)
+        if not transactionExists and not transactionInBlock and signatureValid:
             self.transactionPool.addTransaction(transaction)
             message = Message(self.p2p.socketConnector, "TRANSACTION", transaction)
             encodedMessage = BlockchainUtils.encode(message)
@@ -46,5 +47,12 @@ class Node:
         forger = self.blockchain.nextForger()
         if forger == self.wallet.publicKeyString():
             print("im the next forger")
+            block = self.blockchain.createBlock(
+                self.transactionPool.transactions, self.wallet
+            )
+            self.transactionPool.removeFromPool(block.transactions)
+            message = Message(self.p2p.socketConnector, "BLOCK", block)
+            encodedMessage = BlockchainUtils.encode(message)
+            self.p2p.broadcast(encodedMessage)
         else:
             print("im not the next forger")
