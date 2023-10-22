350176e4315916ec0e9f7c61e2691a1d8a758c5e
arvndblocker"nn <aravindvasang@gmail.com>
2023-10-22 11:53:42 +0530

    pos_testing

--- Interaction.py
+++ Interaction.py
@@ -3,14 +3,24 @@ from BlockchainUtils import BlockchainUtils
 import requests
 
 
-if __name__ == "__main__":
-    bob = Wallet()
-    alice = Wallet()
-    exchange = Wallet()
-
-    transaction = exchange.createTransaction(alice.publicKeyString(), 10, "EXCHANGE")
-
+def postTransaction(sender, receiver, amount, type):
+    transaction = sender.createTransaction(receiver.publicKeyString(), amount, type)
     url = "http://localhost:5000/transaction"
     package = {"transaction": BlockchainUtils.encode(transaction)}
     request = requests.post(url, json=package)
-    print(request.text)
+
+
+if __name__ == "__main__":
+    bob = Wallet()
+    alice = Wallet()
+    alice.fromKey("keys/stakerPrivateKey.pem")
+    exchange = Wallet()
+    john = Wallet()
+
+    # forger: genesis
+    postTransaction(exchange, alice, 100, "EXCHANGE")
+    postTransaction(exchange, bob, 100, "EXCHANGE")
+    postTransaction(alice, alice, 25, "STAKE")
+
+    # forger: chosen by pos
+    postTransaction(bob, john, 1, "TRANSFER")
